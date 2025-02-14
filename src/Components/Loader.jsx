7d7de62d6f4130e2a6cc42a4edb755ba24bfefec
= <=>
2025-02-14 08:36:04 +0530

    updated

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+import { CircularProgress, Box } from "@mui/material";
+
+const Loader = () => {
+    return (
+        <Box className="h-lvh flex justify-center items-center">
+            <CircularProgress />
+        </Box>
+    );
+};
+
+export default Loader;
--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -4,53 +4,68 @@ import Header from './Header'
 import Sidebar from './Sidebar'
 import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
+import Loader from '../Components/Loader';
 
 const Layout = () => {
     const navigate = useNavigate();
-    const {pathname} = useLocation();
+    const { pathname } = useLocation();
+    const [loading, setLoading] = React.useState(true);
     const gettoken = () => {
+        setLoading(true);
         const mtoken = localStorage.getItem(token);
-        if(!mtoken){
+        if (!mtoken) {
             navigate('/login');
         }
+        setLoading(false);
     }
     React.useEffect(() => {
         gettoken();
     }, [pathname]);
-   
+
     return (
         <>
-            <ToastContainer
-                position="top-right"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="colored"
-            />
-            <section>
-                <div className="@container mx-auto">
-                    <div className="grid grid-cols-15">
-                        <div className="col-span-3">
-                            <div className="w-full sticky top-0">
-                                <Sidebar />
+            {
+                loading ? (
+                    <>
+                        <Loader/>
+                    </>
+                ) : (
+                    <>
+
+                        <ToastContainer
+                            position="top-right"
+                            autoClose={5000}
+                            hideProgressBar={false}
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss
+                            draggable
+                            pauseOnHover
+                            theme="colored"
+                        />
+                        <section>
+                            <div className="@container mx-auto">
+                                <div className="grid grid-cols-15">
+                                    <div className="col-span-3">
+                                        <div className="w-full sticky top-0">
+                                            <Sidebar />
+                                        </div>
+                                    </div>
+                                    <div className="col-span-12 bg-gray-100">
+                                        <div className="w-full p-2">
+                                            <Header />
+                                        </div>
+                                        <div className="w-full p-5">
+                                            <Outlet />
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-span-12 bg-gray-100">
-                            <div className="w-full p-2">
-                                <Header />
-                            </div>
-                            <div className="w-full p-5">
-                                <Outlet />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+                        </section>
+                    </>
+                )
+            }
         </>
     )
 }
