c37e6229caee00d99d3c0f34305e977b9290b25d
frantic03117 <shahrukh@franticpro.com>
2025-02-17 17:21:49 +0530

    reception

--- src/pages/appointment/CreateAppointment.jsx
+++ src/pages/appointment/CreateAppointment.jsx
@@ -3,12 +3,64 @@ import React from 'react'
 import { Box, Button, Grid, MenuItem, TextField } from "@mui/material"
 import axios from 'axios';
 import { API_URL, token } from '../../utils';
+import { toast } from 'react-toastify';
 
 const CreateAppointment = () => {
     const [doctors, setDoctors] = React.useState([]);
     const [doc_id, setDocId] = React.useState('');
     const [sdate, setSdate] = React.useState('');
     const mtoken = localStorage.getItem(token);
+    const [slots, setSlots] = React.useState([]);
+    const [slot_id, setSlotId] = React.useState('');
+    const [udata, setUdata] = React.useState({});
+    const [spcl, setSpcl] = React.useState('');
+    const handleUdata = (e) => {
+        const key = e.target.name;
+        const value = e.target.value;
+        setUdata((prev) => ({
+            ...prev,
+            [key]: value
+        }));
+    }
+    const saveAppointment = async () => {
+        const data = {
+            ...udata,
+            "date": sdate,
+            "slot": slot_id,
+            "hospital": doctors.find(itm => itm._id == doc_id)?.hospitals[0],
+            "doctor": doc_id,
+
+            "specialization": spcl,
+            "appointment_fees": doctors.find(itm => itm._id == doc_id)?.specializations.find(obj => obj.specialization._id == spcl)?.price,
+            "consultation_fees": 200,
+            "payment_status": "Success"
+        }
+        const resp = await axios.post(API_URL + "/hospital/receptionist/store-reservation", data, {
+            headers: {
+                Authorization: "Bearer " + mtoken
+            },
+        });
+        if (!resp.data.error) {
+            toast.success('Appointment fixed')
+        }
+    }
+
+    const getslots = async () => {
+        const resp = await axios.get(API_URL + "/hospital/receptionist/doctor-slot", {
+            headers: {
+                Authorization: "Bearer " + mtoken
+            },
+            params: {
+                doctor: doc_id,
+                date: sdate
+            }
+        });
+        console.log(resp);
+        setSlots(resp.data.misc);
+    }
+    const handleSelectSlot = (id) => {
+        setSlotId(id);
+    }
     const getdoctors = async () => {
         const resp = await axios.get(API_URL + "/hospital/receptionist/list/doctors", {
             headers: {
@@ -17,8 +69,9 @@ const CreateAppointment = () => {
         });
         setDoctors(resp.data.data);
     }
-    const handleDocid = (value) => {
-        setDocId(value)
+    const handleDocid = (e) => {
+        setDocId(e.target.value)
+        console.log(e.target.value)
     }
     React.useEffect(() => {
         getdoctors();
@@ -27,13 +80,13 @@ const CreateAppointment = () => {
         <>
             <Box className="p-4 bg-white" borderRadius={3} boxShadow={3}>
                 <Grid container spacing={2}>
-                    <Grid item xs={4}>
+                    <Grid item xs={3}>
                         <TextField
                             select
                             fullWidth
                             label="Select Doctor"
                             name="doctor"
-                            size='small'
+                            size="small"
                             onChange={handleDocid}
                             slotProps={{
                                 inputLabel: {
@@ -41,18 +94,46 @@ const CreateAppointment = () => {
                                 },
                             }}
                         >
-                            {
-                                doctors.filter(itm => itm.is_active).map(itm => (
-                                    <>
-                                        <MenuItem value={itm._id}>{itm.name}</MenuItem>
-                                    </>
-                                ))
-                            }
-
-
+                            {doctors.filter(itm => itm.is_active).map(itm => (
+                                <MenuItem key={itm._id} value={itm._id}>
+                                    {itm.name}
+                                </MenuItem>
+                            ))}
                         </TextField>
+
                     </Grid>
-                    <Grid item xs={4}>
+                    {
+                        doc_id && (
+                            <>
+
+
+                                <Grid item xs={3}>
+                                    <TextField
+                                        select
+                                        fullWidth
+                                        label="Select Problem"
+                                        name="specialization"
+                                        size="small"
+                                        value={spcl}
+                                        onChange={(e) => setSpcl(e.target.value)}
+                                        slotProps={{
+                                            inputLabel: {
+                                                shrink: true,
+                                            },
+                                        }}
+                                    >
+                                        {doctors.find(itm => itm._id == doc_id)?.specializations.map(itm => (
+                                            <MenuItem key={itm.specialization._id} value={itm.specialization._id}>
+                                                {itm.specialization.name}
+                                            </MenuItem>
+                                        ))}
+                                    </TextField>
+
+                                </Grid>
+                            </>
+                        )
+                    }
+                    <Grid item xs={3}>
                         <TextField
                             fullWidth
                             label="Select Appointment Date"
@@ -68,12 +149,67 @@ const CreateAppointment = () => {
                             }}
                         />
                     </Grid>
-                    <Grid item xs={4}>
-                        <Button disabled={!doc_id || !sdate} variant="contained" color="primary.main">Get slots</Button>
+                    <Grid item xs={3}>
+                        <Button onClick={getslots} disabled={!doc_id || !sdate} variant="contained" color="primary.main">Get slots</Button>
                     </Grid>
                 </Grid>
 
             </Box>
+            {
+                slots.length > 0 && (
+                    <>
+                        <Box className="p-4 mt-5 bg-white" borderRadius={3} boxShadow={3}>
+                            <Grid container gap={1}>
+                                {
+                                    slots.map(itm => (
+                                        <>
+                                            <Grid onClick={() => handleSelectSlot(itm._id)} item className={`p-2 cursor-pointer text-xs border border-[var(--primary)] ${slot_id == itm._id ? 'bg-[var(--primary)] text-white' : ''}`} lg={2} xs={2}>
+                                                {itm.name}
+                                            </Grid>
+                                        </>
+                                    ))
+                                }
+                                <Grid item lg={12} ></Grid>
+                                <Grid item lg={3} >
+                                    <TextField
+                                        fullWidth
+                                        label="Enter Name"
+                                        name="user_name"
+                                        type="text"
+                                        size='small'
+                                        onChange={handleUdata}
+
+                                        slotProps={{
+                                            inputLabel: {
+                                                shrink: true,
+                                            },
+                                        }}
+                                    />
+                                </Grid>
+                                <Grid item lg={3} >
+                                    <TextField
+                                        fullWidth
+                                        label="Enter Phone Number"
+                                        name="user_phone"
+                                        type="text"
+                                        size='small'
+                                        onChange={handleUdata}
+
+                                        slotProps={{
+                                            inputLabel: {
+                                                shrink: true,
+                                            },
+                                        }}
+                                    />
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Button onClick={saveAppointment} variant="contained" color="primary.main">Submit</Button>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </>
+                )
+            }
         </>
     )
 }
